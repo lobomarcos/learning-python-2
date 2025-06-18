7926fdcfcde6a0cf0964e274058d2cfb3cb1f7fc
lobomarcosemail <wofeworks@gmail.com>
2025-06-17 21:39:03 -0300

    início do curso!

---
+++ Course 2/011_conditions.py
@@ -0,0 +1,14 @@
+# CONDIÇÕES ANINHADAS
+# else - elif (dá pra usar or, in, and junto do elif) - else
+
+# if - else
+nome = str (input('Qual é o seu nome? '))
+
+if nome == 'Marcos':
+    print ('Seu nome é muito legal!')
+elif nome == 'Pedro' or nome == 'Lucas' or nome == 'Maria':
+    print ('Seu nome é bem popular!')
+else: 
+    print ('Seu nome é beeeem comum...')
+
+print ('Prazer em te conhecer, {}!' .format(nome))
