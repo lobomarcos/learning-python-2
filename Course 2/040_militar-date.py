d21398b9af2d487303cf68aa017945164e20c0a4
lobomarcosemail <wofeworks@gmail.com>
2025-06-19 19:32:23 -0300

    ###

--- Course 2/040_militar-date.py
+++ Course 2/040_militar-date.py
@@ -12,5 +12,4 @@ elif idade == idadeAlistamento:
     print ('Você tem {} anos. Já está na hora do seu alistamento militar.' .format (idadeAlistamento))
 else:
     print ('Você tem {} anos. Já se passaram {} anos do mommento de se alistar para o exército.' .format(idade, idade - idadeAlistamento))
-
-# PAREI EM 20 MIN
+    
