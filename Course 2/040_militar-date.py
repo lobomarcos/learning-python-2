474a4b91f3d25d008e7fd1b400fee8fbcce789bc
lobomarcosemail <wofeworks@gmail.com>
2025-06-18 22:06:54 -0300

    ITS MILITAR TIME

---
+++ Course 2/040_militar-date.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+anoNascimento = int (input('Informe o ano de seu nascimento: '))
+
+hoje = date.today().year
+idade = hoje - anoNascimento
+idadeAlistamento = 18
+
+if idade < idadeAlistamento:
+    print ('Você tem {} anos. Faltam {} anos para o momento de você se alistar para o exército.' .format (idade, idadeAlistamento - idade))
+elif idade == idadeAlistamento:
+    print ('Você tem {} anos. Já está na hora do seu alistamento militar.' .format (idadeAlistamento))
+else:
+    print ('Você tem {} anos. Já se passaram {} anos do mommento de se alistar para o exército.' .format(idade, idade - idadeAlistamento))
