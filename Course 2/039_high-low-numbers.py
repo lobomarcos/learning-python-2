6f6a36bcda5dca7ebf506b272c9f328d27af992b
lobomarcosemail <wofeworks@gmail.com>
2025-06-23 17:16:18 -0300

    else

--- Course 2/039_high-low-numbers.py
+++ Course 2/039_high-low-numbers.py
@@ -5,5 +5,5 @@ if n1 > n2:
     print ('{} é maior que {}.' .format(n1, n2))
 elif n2 > n1:
     print ('{} é maior que {}.' .format(n2, n1))
-elif n1 == n2:
+else:
     print ('Não tem um número maior. {} é igual a {}.' .format(n1, n2))
