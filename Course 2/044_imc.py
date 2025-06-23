5b1c9855ecfe108fea79b5c63205c09dd6eebe42
lobomarcosemail <wofeworks@gmail.com>
2025-06-23 17:43:50 -0300

    fix

--- Course 2/044_imc.py
+++ Course 2/044_imc.py
@@ -1,7 +1,7 @@
 peso = float (input('Digite seu peso: '))
 altura = float (input('Digite sua altura: '))
 
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 
 if imc < 18.5:
     print ('Seu imc é {:.1f}. Você está abaixo do peso.' .format(imc))
