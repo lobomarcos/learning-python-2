f48518cef92d12f0e5570980a75e149bd86da311
lobomarcosemail <wofeworks@gmail.com>
2025-06-25 18:36:33 -0300

    ###

--- Course 2/047_timer.py
+++ Course 2/047_timer.py
@@ -1,19 +1,9 @@
 from time import sleep
 
 # TIMER DE 10 A 0 - CONTAGEM REGRESSIVA
-'''
+
 for t in range (10, -1, -1):
     print (t)
     sleep(1)
-
-print ('Fogos!')
-'''
-
-# TIMER - CONTAGEM REGRESSIVA - SELECIONÁVEL
-
-start = int (input('Digite o tempo desejado para a contagem regressiva: '))
-for t in range(start, -1, -1):
-    print (t)
-    sleep (1)
 
 print ('Fogos!')
