6c6f41751707777ed6bb5634cbfaafb145bd2afa
lobomarcosemail <wofeworks@gmail.com>
2025-06-25 19:08:36 -0300

    COUNTING PAIRS

--- Course 2/048.1_pair.py
+++ Course 2/048.1_pair.py
@@ -1,3 +1,14 @@
 # VERIFICANDO NÚMEROS PARES EM UM INTERVALO DETERMINADO PELO USUÁRIO.
 
 insert = int (input('Qual o número inicial?: '))
+insert2 = int (input('Qual o número final?: '))
+
+count = 0
+
+for n in range (insert, insert2 + 1):
+    if n % 2 == 0:
+        print ('é par!')
+        count += 1
+    print (n)
+
+print('Quantidade de números pares:', count)
