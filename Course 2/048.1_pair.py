8e313f5b04487d21d146b57918e56a9ea511c6b7
lobomarcosemail <wofeworks@gmail.com>
2025-06-25 18:52:51 -0300

    pairs

---
+++ Course 2/048.1_pair.py
@@ -0,0 +1,3 @@
+# VERIFICANDO NÚMEROS PARES EM UM INTERVALO DETERMINADO PELO USUÁRIO.
+
+insert = int (input('Qual o número inicial?: '))
--- Course 2/048_pair.py
+++ Course 2/048_pair.py
@@ -1,3 +1,5 @@
+# VERIFICANDO NÚMEROS PARES EM UM INTERVALO DETERMINADO.
+
 for n in range (1,51):
     if n % 2 == 0:
         print ('Par!')
