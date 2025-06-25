6b447ff9559920fffb5f58b25e05c398bdf74d67
lobomarcosemail <wofeworks@gmail.com>
2025-06-25 19:12:03 -0300

    FIX

--- Course 2/048.1_pair.py
+++ Course 2/048.1_pair.py
@@ -7,8 +7,7 @@ count = 0
 
 for n in range (insert, insert2 + 1):
     if n % 2 == 0:
-        print ('é par!')
+        print ('{} é par!' .format(n))
         count += 1
-    print (n)
 
 print('Quantidade de números pares:', count)
