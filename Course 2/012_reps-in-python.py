506f02237f10b493d5ac20ad173d56d351f60887
lobomarcosemail <wofeworks@gmail.com>
2025-06-25 18:05:08 -0300

    ###

--- Course 2/012_reps-in-python.py
+++ Course 2/012_reps-in-python.py
@@ -1,7 +1,7 @@
 # REPS IN PYTHON - FOR 
 
 # 'REPETIR A VARIÁVEL DE CONTROLE NO INTERVALO DETERMINADO.'
-for c in range (1, 11):
+for c in range (0, 10):
     # 'SE HOUVER A COONDIÇÃO, EXECUTAR O COMANDO.'
     if c % 2 == 0:
         print ('Par!')
