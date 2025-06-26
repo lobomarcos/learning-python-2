8e328f4b13c82746b997af53887425a0d842e3c3
lobomarcosemail <wofeworks@gmail.com>
2025-06-26 19:28:07 -0300

    fix

--- Course 2/049.1_no-pairs.py
+++ Course 2/049.1_no-pairs.py
@@ -4,9 +4,11 @@ first = int (input('Digite o número inicial: '))
 last = int (input('Digite o número final: '))
 
 soma = 0
+cont = 0
 
 for n in range (first, last):
     if n % 3 == 0 and n % 2 != 0:
-        soma += n
+        cont = cont + 1
+        soma = soma + n
 
-print (soma)
+print ('A soma dos {} valores solicitados que ficam entre {} e {} é igual a: {}' .format(cont, first, last, soma))
