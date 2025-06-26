d8c36caa8c739a2018230ccced477c1ee6eccdd3
lobomarcosemail <wofeworks@gmail.com>
2025-06-26 19:41:55 -0300

    fix

--- Course 2/051_sum-pairs.py
+++ Course 2/051_sum-pairs.py
@@ -1,8 +1,10 @@
 sumPairs = 0
+count = 0
 
 for x in range (1, 7):
     num = int (input('Digite o {}º número inteiro: ' .format(x)))
     if num % 2 == 0:
         sumPairs = sumPairs + num
+        count = count + 1
 
-print ('A soma dos números pares de é: {}' .format(sumPairs))
+print ('A soma dos números {} pares informados é: {}' .format(count, sumPairs))
