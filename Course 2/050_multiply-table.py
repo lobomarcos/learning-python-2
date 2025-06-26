5f9ab00689a75ec59903c8c66835ddfc5f10ceaf
lobomarcosemail <wofeworks@gmail.com>
2025-06-26 16:47:06 -0300

    MULTIPLY TABLE V2

--- Course 2/050_multiply-table.py
+++ Course 2/050_multiply-table.py
@@ -1,1 +1,6 @@
-# PAREI EM 29 MIN
+n = int (input('Digite um número para saber a sua tabuada: '))
+print ('A tabuada de {} é: ' .format(n))
+
+for x in range (0, 11):
+    multiply = n * x
+    print ('{} x {} = {}' .format (n, x, multiply))
