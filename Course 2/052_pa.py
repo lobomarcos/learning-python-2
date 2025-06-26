b91b2604d988a09b864a6c118077c244415d7b4f
lobomarcosemail <wofeworks@gmail.com>
2025-06-26 19:51:05 -0300

    FIX

--- Course 2/052_pa.py
+++ Course 2/052_pa.py
@@ -1,20 +1,8 @@
-def progressaoAritmetica (firstTerm, ratio, qTerms):
-
-  pa = []
-  actualTerm = firstTerm
-
-  for n in range (qTerms):
-    pa.append (actualTerm)
-    actualTerm = actualTerm + ratio
-
-  return pa
-
 firstTerm = int (input('Digite o primeiro termo da progressão aritmética: '))
 ratio = int (input('Digite a razão da progressão aritmética: '))
-qTerms = int (input('Digite a quantidade de termos da progressão aritmética: '))
+q = int (input('Digite a quantidade de termos da progressão aritmética: '))
 
-pa = progressaoAritmetica (firstTerm, ratio, qTerms)
-print ('A progressão aritmética é:', pa)
+qTerms = firstTerm + (q - 1) * ratio
 
-for x in range (0, qTerms):
-    pa = firstTerm + (qTerms - 1) * ratio
+for x in range (firstTerm, qTerms + ratio, ratio):
+    print (x, end = ' -> ')
