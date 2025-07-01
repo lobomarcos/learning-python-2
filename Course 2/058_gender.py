f010d0b06b6b92c2408a526e23794e303ceffaff
lobomarcosemail <wofeworks@gmail.com>
2025-07-01 11:43:26 -0300

    FIX

--- Course 2/058_gender.py
+++ Course 2/058_gender.py
@@ -1,7 +1,7 @@
-g = input('Com qual sexo você se identifica? [M / F] ').upper()
+g = input('Com qual sexo você se identifica? [M / F] ').strip().upper()[0]
 
 while g != 'M' and g != 'F':
     print('Essa opção não existe, tente novamente.')
-    g = input('Com qual sexo você se identifica? [M / F] ').upper()
+    g = input('Com qual sexo você se identifica? [M / F] ').strip().upper()[0]
 
 print('Você digitou:', g)
