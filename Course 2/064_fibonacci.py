a9ae9acead2226e86487e779421b3da248b055d0
lobomarcosemail <wofeworks@gmail.com>
2025-07-08 10:39:36 -0300

    fibonacci

---
+++ Course 2/064_fibonacci.py
@@ -0,0 +1,17 @@
+from math import ceil
+
+n = int (input('Quantos números da Sequência de Fibonnaci você quer mostrar? '))
+
+t1 = 0
+t2 = 1
+count = 3
+
+print ('Os {} primeiros números da Sequência de Fibonacci é: ' .format(n))
+print ('{} -> {} -> ' .format(t1, t2), end = '')
+while count <= n:
+    t3 = t1 + t2
+    print ('{} -> ' .format(t3), end = '')
+    t1 = t2
+    t2 = t3
+    count = count + 1
+print ('Fim!')
