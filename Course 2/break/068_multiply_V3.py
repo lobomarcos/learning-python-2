f0c8e7c24427e05b12e54640ca1c8c077197ac15
lobomarcosemail <wofeworks@gmail.com>
2025-07-10 16:41:30 -0300

    O IF NUMBER < 0 NAO FUNCIONA

--- Course 2/break/068_multiply_V3.py
+++ Course 2/break/068_multiply_V3.py
@@ -3,10 +3,14 @@ number = int (input('Digite um número para saber a sua tabuada: '))
 if number > 0:
     print ('A tabuada de {} é: ' .format(number))
 
-for x in range (0, 11):
+while number > 0:
     if number < 0:
         print ('Número negativo não possui tabuada.')
         break
-    
-    multiply = number * x
-    print ('{} x {} = {}' .format (number, x, multiply))
+        
+    for x in range (0, 11):
+
+        multiply = number * x
+        print ('{} x {} = {}' .format (number, x, multiply))
+
+    number = int (input('De qual outro número você gostaria de ver a tabuada? '))
