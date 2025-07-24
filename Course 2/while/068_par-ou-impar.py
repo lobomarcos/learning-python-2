55f5b7bcaf0e94b383ca9684558c60ca99ab7f37
lobomarcosemail <wofeworks@gmail.com>
2025-07-24 20:54:21 -0300

    ###

--- Course 2/while/068_par-ou-impar.py
+++ Course 2/while/068_par-ou-impar.py
@@ -9,7 +9,8 @@ while True:
     while tipo not in 'PI':
         tipo = str (input('Você escolhe Ímpar ou Par? [I/P] ')).strip().upper()[0]
 
-    print(f'Você jogou {jogador} e o computador jogou {computador}. Total de: {total}')
+    print (f'Você jogou {jogador} e o computador jogou {computador}. Total de: {total}')
+    print ('Deu par!' if total % 2 == 0 else 'Deu ímpar!')
 
     if tipo == 'P':
         if total % 2 == 0:
