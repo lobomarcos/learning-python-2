17ea8710b802ec1510cd0544e89dee8ad74a7af3
lobomarcosemail <wofeworks@gmail.com>
2025-07-24 21:25:49 -0300

    ###

--- Course 2/break/070_full.py
+++ Course 2/break/070_full.py
@@ -1,8 +1,7 @@
 print ('CADASTRO DE PESSOAS')
-
+'''
 sM = sF = 0
 i = iM = iF = 0
-
 while True:
     sexo = str (input('Você se identifica com qual gênero? [M/F] ')).strip().upper()[0]
     idade = int (input('Quantos anos você tem? '))
@@ -24,4 +23,30 @@ while True:
 
     if opt == 'N':
         break
+'''
+tot18 = toth = totm20 = 0
+
+while True:
+    idade = int (input('Idade: '))
+    sexo = ' '
+
+    while sexo not in 'MF':
+        sexo = str (input('Com qual sexo você se identifica? [M/F] ')).strip().upper()[0]
+    if idade >= 18:
+        tot18 = tot18 + 1
+    if sexo == 'M':
+        toth = toth + 1
+    if sexo == 'F' and idade < 20:
+        totm20 = totm20 + 1
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str (input('Quer continuar? [S/N] ')).strip().upper()[0]
     
+    if resp == 'N':
+        break
+    
+print ('Pessoas com mais de 18 anos {}.' .format(tot18))
+print ('Homens cadastrados: {}.' .format(toth))
+print ('Mulheres com menos de 20 anos: {}.' .format(totm20))
+print ('Acabou.')
